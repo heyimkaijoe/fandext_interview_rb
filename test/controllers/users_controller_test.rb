48222f3283ac3dee67cf7d97f42a5abd5fd970c4
kchsu <kevin3117213@gmail.com>
2023-01-31 18:09:22 +0800

    users_controller_test done

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,8 +11,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create user succeeded" do
+    post users_path, params: {user: {email: 'kevin3117213@gmail.com', first_name: 'KAI CHIEH', last_name: 'HSU', address: '南京西路十段'}}
+    assert_redirected_to users_path
+    assert_equal 'Welcome to fandnext', flash[:success]
   end
 
   test "create user failed" do
+    post users_path, params: {user: {email: 'invalid_email', first_name: 'John', last_name: 'Doe', address: '123 Easy St'}}
+    assert_template :new
+    assert_equal 'Domain invalid', flash[:warning]
   end
 end
